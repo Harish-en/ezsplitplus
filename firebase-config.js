a654dff89f68fd37cbcfb3641afc3768088a426a
Harish Enzinzo <enzinzoharish@gmail.com>
2025-04-02 22:33:30 +0700

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,13 +1,21 @@
-// Cấu hình Firebase
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "YOUR_API_KEY",
-    authDomain: "YOUR_AUTH_DOMAIN",
-    projectId: "YOUR_PROJECT_ID",
-    storageBucket: "YOUR_STORAGE_BUCKET",
-    messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-    appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyCXDgnm-taNPUDZNRK1YuHmfUV0Pu7BDPk",
+  authDomain: "ezsplit-plus.firebaseapp.com",
+  projectId: "ezsplit-plus",
+  storageBucket: "ezsplit-plus.firebasestorage.app",
+  messagingSenderId: "1082075090740",
+  appId: "1:1082075090740:web:cf424da6cac9dd652e5f61",
+  measurementId: "G-KEDEKV4RY3"
 };
 
-// Khởi tạo Firebase
-firebase.initializeApp(firebaseConfig);
-const db = firebase.firestore(); 
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
